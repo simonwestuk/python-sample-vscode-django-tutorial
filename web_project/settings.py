905697f4ac279ee21f693f3c3b3f7cff4bff8d94
Simon West <43855704+simonwestuk@users.noreply.github.com>
2024-02-03 19:27:04 +0000

    added db

--- web_project/settings.py
+++ web_project/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 # a specific host, you must also add 'localhost' and/or '127.0.0.1' for local
 # debugging (which are enabled by default when ALLOWED_HOSTS is empty.)
 ALLOWED_HOSTS = [
-    #'localhost',
-    #'127.0.0.1'
+    'localhost',
+    '127.0.0.1'
     #'vscode-django-tutorial.azurewebsites.net'  # Sample name only!
 ]
 
